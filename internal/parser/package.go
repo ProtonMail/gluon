2ec9f356d9f4b121b91907ef9a7e564388dbe2bd
LBeernaertProton <104621424+LBeernaertProton@users.noreply.github.com>
2022-06-01 17:35:17 +0200

    build: Fix arm64 build on Mac OS (#2)
    Add missing binaries and update package.go.
    
    Update github actions to build Mac OS x64 and arm64 binaries.
    
    Update vcpkg to only build release version of dependencies on CI.

--- internal/parser/package.go
+++ internal/parser/package.go
@@ -3,6 +3,7 @@ package parser
 //#cgo LDFLAGS: -lparser -lparser_gen -lproto -lprotobuf -lantlr4-runtime
 //#cgo linux,amd64   LDFLAGS: -Llib/Linux/x86_64   -luuid
 //#cgo darwin,amd64  LDFLAGS: -Llib/Darwin/x86_64  -framework CoreFoundation
+//#cgo darwin,arm64  LDFLAGS: -Llib/Darwin/arm64  -framework CoreFoundation
 //#cgo windows,amd64 LDFLAGS: -Llib/Windows/AMD64 -lOle32 -static-libgcc -static-libstdc++ -static
 import "C"
 
