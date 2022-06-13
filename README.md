34ceba68a8a122aaa29c254b0f08182da7446d51
James Houlahan <james.houlahan@proton.ch>
2022-06-13 12:37:46 +0200

    docs: Update README.md with info about demo

--- README.md
+++ README.md
@@ -9,3 +9,45 @@
 <a href="LICENSE"><img src="https://img.shields.io/github/license/ProtonMail/gluon.svg" alt="License"></a>
 </p>
 
+# Demo
+
+The demo server can be started with:
+
+```bash
+$ go run demo/demo.go
+INFO[0000] User added to server                          userID=d5a706ae-c7bf-4cfd-bad3-982eafcdfe39
+INFO[0000] User added to server                          userID=622e121e-c9c7-43f9-b0ee-22bf868e8429
+INFO[0000] Server is listening on 127.0.0.1:1143
+```
+
+The demo server includes two demo users,
+the first has addresses `user1@example.com` and `alias1@example.com` and password `password1`,
+the second has addresses `user2@example.com` and `alias2@example.com` and password `password2`:
+
+```
+telnet 127.0.0.1 1143
+Trying 127.0.0.1...
+Connected to 127.0.0.1.
+Escape character is '^]'.
+* OK [CAPABILITY IDLE IMAP4rev1 MOVE UIDPLUS UNSELECT] gluon session ID 1
+tag login user1@example.com password1
+tag OK [CAPABILITY IDLE IMAP4rev1 MOVE UIDPLUS UNSELECT] (^_^)
+
+...
+
+tag login alias1@example.com password1
+tag OK [CAPABILITY IDLE IMAP4rev1 MOVE UIDPLUS UNSELECT] (^_^)
+
+...
+
+tag login user2@example.com password2
+tag OK [CAPABILITY IDLE IMAP4rev1 MOVE UIDPLUS UNSELECT] (^_^)
+
+...
+
+tag login alias2@example.com password2
+tag OK [CAPABILITY IDLE IMAP4rev1 MOVE UIDPLUS UNSELECT] (^_^)
+```
+
+The demo accounts contain no messages. You can connect an IMAP client (e.g. thunderbird) and use it to copy in
+messages from another mail server.
--- demo/demo.go
+++ demo/demo.go
@@ -18,48 +18,70 @@ import (
 )
 
 func main() {
-	dir, err := os.MkdirTemp("", "")
-	if err != nil {
-		logrus.WithError(err).Fatal("Failed to make temporary directory")
-	}
-
-	server, err := gluon.New(filepath.Join(dir, "server"))
+	server, err := gluon.New(temp())
 	if err != nil {
 		logrus.WithError(err).Fatal("Failed to create server")
 	}
 
+	if err := addUser(server, []string{"user1@example.com", "alias1@example.com"}, "password1"); err != nil {
+		logrus.WithError(err).Fatal("Failed to add user")
+	}
+
+	if err := addUser(server, []string{"user2@example.com", "alias2@example.com"}, "password2"); err != nil {
+		logrus.WithError(err).Fatal("Failed to add user")
+	}
+
+	listener, err := net.Listen("tcp", "localhost:1143")
+	if err != nil {
+		logrus.WithError(err).Fatal("Failed to listen")
+	}
+
+	logrus.Infof("Server is listening on %v", listener.Addr())
+
+	for err := range server.Serve(context.Background(), listener) {
+		logrus.WithError(err).Error("Error while serving")
+	}
+}
+
+func addUser(server *gluon.Server, addresses []string, password string) error {
 	connector := connector.NewDummy(
-		[]string{"user@example.com", "alias@example.com"},
-		"password",
+		addresses,
+		password,
 		time.Second,
 		imap.NewFlagSet(),
 		imap.NewFlagSet(),
 		imap.NewFlagSet(),
 	)
 
-	store, err := store.NewOnDiskStore(filepath.Join(dir, "store"), []byte("passphrase"))
+	store, err := store.NewOnDiskStore(temp(), []byte("passphrase"))
 	if err != nil {
-		logrus.WithError(err).Fatal("Failed to create store")
+		return err
 	}
 
 	userID, err := server.AddUser(
 		connector,
 		store,
 		dialect.SQLite,
-		fmt.Sprintf("file:%v?cache=shared&_fk=1", filepath.Join(dir, fmt.Sprintf("%v.db", "userID"))),
+		fmt.Sprintf("file:%v?cache=shared&_fk=1", filepath.Join(temp(), fmt.Sprintf("%v.db", "userID"))),
 	)
 	if err != nil {
-		logrus.WithError(err).Fatal("Failed to add user")
+		return err
+	}
+
+	if err := connector.Sync(context.Background()); err != nil {
+		return err
 	}
 
 	logrus.WithField("userID", userID).Info("User added to server")
 
-	listener, err := net.Listen("tcp", ":1143")
+	return nil
+}
+
+func temp() string {
+	temp, err := os.MkdirTemp("", "")
 	if err != nil {
-		logrus.WithError(err).Fatal("Failed to listen")
+		panic(err)
 	}
 
-	for err := range server.Serve(context.Background(), listener) {
-		logrus.WithError(err).Error("Error while serving")
-	}
+	return temp
 }
