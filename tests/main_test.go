c28b059598e8b87c5f7cf48a3c9eb649b9af64f7
James Houlahan <james.houlahan@proton.ch>
2022-06-14 14:59:35 +0200

    chore: Rename GOMSRV to GLUON

--- tests/main_test.go
+++ tests/main_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	os.Setenv("GOMSRV_TEST_HOST", "127.0.0.1:2143")
-	os.Setenv("GOMSRV_TEST_USER", "dummy@proton.ch")
-	os.Setenv("GOMSRV_TEST_PASS", "password")
+	os.Setenv("GLUON_TEST_HOST", "127.0.0.1:2143")
+	os.Setenv("GLUON_TEST_USER", "dummy@proton.ch")
+	os.Setenv("GLUON_TEST_PASS", "password")
 
 	os.Exit(m.Run())
 }
