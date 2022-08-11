e2696451ceda090356e06ede97c1e37964b8ca35
Leander Beernaert <leander.beernaert@proton.ch>
2022-08-11 09:36:56 +0200

    Fix: Correct store create benchmark
    It was not using the same file size as the rest of the benchmarks.

--- benchmarks/gluon_bench/store_benchmarks/create.go
+++ benchmarks/gluon_bench/store_benchmarks/create.go
@@ -2,13 +2,10 @@ package store_benchmarks
 
 import (
 	"context"
-	"math/rand"
-
 	"github.com/ProtonMail/gluon/benchmarks/gluon_bench/benchmark"
 	"github.com/ProtonMail/gluon/benchmarks/gluon_bench/flags"
 	"github.com/ProtonMail/gluon/benchmarks/gluon_bench/reporter"
 	"github.com/ProtonMail/gluon/benchmarks/gluon_bench/timing"
-	"github.com/ProtonMail/gluon/benchmarks/gluon_bench/utils"
 	"github.com/ProtonMail/gluon/store"
 	"github.com/google/uuid"
 )
@@ -29,12 +26,11 @@ func (*Create) TearDown(ctx context.Context, store store.Store) error {
 
 func (*Create) Run(ctx context.Context, st store.Store) (*reporter.BenchmarkRun, error) {
 	return RunStoreWorkers(ctx, st, func(ctx context.Context, s store.Store, dc *timing.Collector, u uint) error {
-		messages := []string{utils.MessageAfterNoonMeeting, utils.MessageMultiPartMixed, utils.MessageEmbedded}
-		messagesLen := len(messages)
+		data := make([]byte, *flags.StoreItemSize)
 
 		for i := uint(0); i < *flags.StoreItemCount; i++ {
 			dc.Start()
-			err := s.Set(uuid.NewString(), []byte(messages[rand.Intn(messagesLen)]))
+			err := s.Set(uuid.NewString(), data)
 			dc.Stop()
 
 			if err != nil {
