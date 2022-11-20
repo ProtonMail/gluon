21bac3aea95ccf7f7b120f28181172c6441f242d
James Houlahan <james.houlahan@proton.ch>
2022-11-20 21:51:44 +0100

    refactor: Cleaner session command handling

--- internal/liner/liner.go
+++ internal/liner/liner.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"regexp"
 	"strconv"
+	"sync"
 
 	"github.com/google/uuid"
 )
@@ -15,16 +16,53 @@ var rxLiteral = regexp.MustCompile(`\{(\d+)\}\r\n$`)
 
 type Liner struct {
 	br *bufio.Reader
+	mu sync.RWMutex
+}
+
+type Line struct {
+	Line     []byte
+	Literals map[string][]byte
 }
 
 func New(r io.Reader) *Liner {
 	return &Liner{br: bufio.NewReader(r)}
 }
 
+// Reset resets the liner to read from a new reader.
+func (l *Liner) Reset(r io.Reader) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.br.Reset(r)
+}
+
+// Lines returns a channel that will receive lines as they are read.
+func (l *Liner) Lines(doContinuation func() error) <-chan Line {
+	ch := make(chan Line)
+
+	go func() {
+		defer close(ch)
+
+		for {
+			line, lits, err := l.Read(doContinuation)
+			if err != nil {
+				return
+			}
+
+			ch <- Line{line, lits}
+		}
+	}()
+
+	return ch
+}
+
 // Read reads a full line, automatically reading again if the line was not complete.
 // Each time an additional read is performed, doContinuation is called.
 // If the callback returns an error, the operation is aborted.
 func (l *Liner) Read(doContinuation func() error) ([]byte, map[string][]byte, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
 	line, err := l.br.ReadBytes('\n')
 	if err != nil {
 		return nil, nil, err
--- internal/session/command.go
+++ internal/session/command.go
@@ -2,9 +2,7 @@ package session
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"io"
 
 	"github.com/bradenaw/juniper/xslices"
 	"golang.org/x/exp/maps"
@@ -28,19 +26,25 @@ func (s *Session) startCommandReader(ctx context.Context, del string) <-chan com
 		defer close(cmdCh)
 
 		for {
-			tag, cmd, err := s.readCommand(del)
-			if err != nil && !errors.Is(err, io.EOF) {
+			line, literals, err := s.liner.Read(func() error { return response.Continuation().Send(s) })
+			if err != nil {
+				return
+			}
+
+			s.logIncoming(string(line), xslices.Map(maps.Keys(literals), func(k string) string {
+				return fmt.Sprintf("%v: '%s'", k, literals[k])
+			})...)
+
+			tag, cmd, err := parse(line, literals, del)
+			if err != nil {
 				reporter.MessageWithContext(ctx,
 					"Failed to parse imap command",
 					reporter.Context{"error": err},
 				)
-			}
-
-			if err == nil && cmd.GetStartTLS() != nil {
+			} else if cmd.GetStartTLS() != nil {
 				// TLS needs to be handled here to ensure that next command read is over the TLS connection.
-				if startTLSErr := s.handleStartTLS(tag, cmd.GetStartTLS()); startTLSErr != nil {
-					cmd = nil
-					err = startTLSErr
+				if err = s.handleStartTLS(tag, cmd.GetStartTLS()); err != nil {
+					return
 				} else {
 					continue
 				}
@@ -60,28 +64,4 @@ func (s *Session) startCommandReader(ctx context.Context, del string) <-chan com
 	})
 
 	return cmdCh
-}
-
-func (s *Session) readCommand(del string) (string, *proto.Command, error) {
-	line, literals, err := s.liner.Read(func() error { return response.Continuation().Send(s) })
-	if err != nil {
-		return "", nil, err
-	}
-
-	if len(literals) == 0 {
-		s.logIncoming(string(line))
-	} else {
-		s.logIncoming(fmt.Sprintf("%v Literals: %v", string(line),
-			xslices.Map(maps.Keys(literals), func(k string) string {
-				return fmt.Sprintf("%v: '%v'", k, string(literals[k]))
-			}),
-		))
-	}
-
-	tag, cmd, err := parse(line, literals, del)
-	if err != nil {
-		return tag, cmd, err
-	}
-
-	return tag, cmd, nil
 }
