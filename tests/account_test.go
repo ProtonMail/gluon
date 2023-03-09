65db9f3ab739e5bc0237422a5a91cfb021699714
Leander Beernaert <leander.beernaert@proton.ch>
2023-03-09 11:14:44 +0100

    fix(GODT-2442): Fix problem introduce in code review
    The glob pattern '*' was missing from the file path. Add unit test to
    catch issue in the future.

---
+++ tests/account_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"errors"
+	"github.com/ProtonMail/gluon/internal/db"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestAccountRemovalMovesDBToDeferredDeleteFolder(t *testing.T) {
+	dataDir := t.TempDir()
+	dbDir := t.TempDir()
+
+	dbDeleteDirPath := db.GetDeferredDeleteDBPath(dbDir)
+
+	// Deferred delete folder does not exist.
+	empty, err := isEmptyFolder(dbDeleteDirPath)
+	require.NoError(t, err)
+	require.True(t, empty)
+
+	runServer(t, defaultServerOptions(t, withDataDir(dataDir), withDatabaseDir(dbDir)), func(session *testSession) {
+		userID := session.removeAccount(t, "user")
+
+		// Remove account's DB was moved to this folder.
+		empty, err := isEmptyFolder(dbDeleteDirPath)
+		require.NoError(t, err)
+		require.False(t, empty)
+
+		// DB dir should not have user id files anymore.
+		matchingFiles, err := filepath.Glob(filepath.Join(dbDir, userID+"*"))
+		require.NoError(t, err)
+		require.Empty(t, matchingFiles)
+	})
+
+	// Database will be deleted on server startup.
+	runServer(t, defaultServerOptions(t, withDataDir(dataDir), withDatabaseDir(dbDir)), func(session *testSession) {
+		empty, err := isEmptyFolder(dbDeleteDirPath)
+		require.NoError(t, err)
+		require.True(t, empty)
+	})
+}
+
+func isEmptyFolder(path string) (bool, error) {
+	matchingFiles, err := filepath.Glob(filepath.Join(path, "*"))
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return true, nil
+		}
+
+		return false, err
+	}
+
+	return len(matchingFiles) == 0, nil
+}
